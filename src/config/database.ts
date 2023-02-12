cada8c25abf4d447896754dfcc75e0afb0bafe70
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-02-12 12:34:11 +0500

    feat: added config

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+const TypeOrmDataSource = new DataSource({
+  type: "postgres",
+  url: process.env.DB_URL,
+  name: "default",
+  entities: ["src/**/*.entity.*"],
+  logging: true,
+  synchronize: true,
+});
+
+export default TypeOrmDataSource;
